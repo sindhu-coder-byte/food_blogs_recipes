57c5e46264ba25dacf8c4edc0cc28f0f677b624b
sindhu-coder-byte <projects_github_email@example.com>
2026-03-23 13:27:09 +0530

    add postgres driver

--- myproject/settings.py
+++ myproject/settings.py
@@ -21,7 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ["food-blogs-recipes-2.onrender.com"]
+ALLOWED_HOSTS = [
+    "food-blogs-recipes-2.onrender.com",
+    "127.0.0.1",
+    "localhost"
+]
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
