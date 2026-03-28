9f50fcfe3987856856d91a2d47c6e12d2df33bed
sindhu-coder-byte <projects_github_email@example.com>
2026-03-28 15:06:01 +0530

    Fix Cloudinary config keys for media upload

--- myproject/settings.py
+++ myproject/settings.py
@@ -43,9 +43,9 @@ CSRF_TRUSTED_ORIGINS = [
 
 
 CLOUDINARY_STORAGE = { 
-    'CLOUDINARY_CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME', ''),
-    'CLOUDINARY_API_KEY': os.getenv('CLOUDINARY_API_KEY', ''),
-    'CLOUDINARY_API_SECRET': os.getenv('CLOUDINARY_API_SECRET', ''),
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME', ''),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY', ''),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET', ''),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
